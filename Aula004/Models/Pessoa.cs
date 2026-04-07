3aac478ea6b992f1c153cb3c8a67114f2b99f4d4
240379 <240379@LTI5403>
2026-04-07 16:33:11 -0300

    Adicionar arquivos de projeto.

---
+++ Aula004/Models/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Aula004.Models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public static List<Pessoa> Lista
+        {
+            get
+            {
+                //var lista = new List<Pessoa>();
+                //lista.Add(new Pessoa { Nome = "Haroldo", Idade = 31 });
+                //lista.Add(new Pessoa { Nome = "Armando", Idade = 7 });
+                //lista.Add(new Pessoa { Nome = "Abraham", Idade = 152 });
+                var lista = new List<Pessoa>
+                {
+                new () { Nome = "Haroldo", Idade = 31},
+                new () { Nome = "Armando", Idade = 7 },
+                new () { Nome = "Abraham", Idade = 152}
+
+                };
+                return lista;
+            }
+        }
+    }
+}
