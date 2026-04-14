8e7fea57d981d8ae06a148f52e47d379b143663b
Vinicius Morimoto <viniciusenzomorimoto2008@gmail.com>
2026-04-14 20:51:59 -0300

    Venda de Imoveis

---
+++ Aula004/Controllers/ImoveisController.cs
@@ -0,0 +1,128 @@
+﻿using Aula004.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Aula4.Controllers
+{
+    public class ImoveisController : Controller
+    {
+        // GET: ImoveisController
+        public ActionResult Index()
+        {
+            var lista = LerLista();
+            return View(lista);
+        }
+
+        private List<Imoveis> LerLista()
+        {
+            List<Imoveis> lista;
+            string imoveis = HttpContext.Session.GetString("imoveis");
+
+            if (string.IsNullOrEmpty(imoveis))
+                lista = IniciarLista();
+            else
+            {
+                lista = JsonConvert.DeserializeObject<List<Imoveis>>(imoveis);
+
+                if (lista == null || lista.Count == 0)
+                    lista = IniciarLista();
+            }
+
+            return lista;
+        }
+
+        private List<Imoveis> IniciarLista()
+        {
+            List<Imoveis> lista = Imoveis.Lista;
+            AtualizarLista(lista);
+            return lista;
+        }
+
+        private void AtualizarLista(List<Imoveis> lista)
+        {
+            string imoveis = JsonConvert.SerializeObject(lista);
+            HttpContext.Session.SetString("imoveis", imoveis);
+        }
+
+        // GET: ImoveisController/Details/5
+        public ActionResult Details(int index)
+        {
+            var lista = LerLista();
+            return View(lista[index]);
+        }
+
+        // GET: ImoveisController/Create
+        public ActionResult Create()
+        {
+            return View(new Imoveis());
+        }
+
+        // POST: ImoveisController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Imoveis imoveis)
+        {
+            try
+            {
+                var lista = LerLista();
+                lista.Add(imoveis);
+                AtualizarLista(lista);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ImoveisController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(LerLista()[id]);
+        }
+
+        // POST: ImoveisController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Imoveis imoveis)
+        {
+            try
+            {
+                var lista = LerLista();
+                lista[id] = imoveis;
+                AtualizarLista(lista);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ImoveisController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var lista = LerLista();
+            return View(lista[id]);
+        }
+
+        // POST: ImoveisController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Imoveis imoveis)
+        {
+            try
+            {
+                var lista = LerLista();
+                lista.RemoveAt(id);
+                AtualizarLista(lista);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Aula004/Models/Imoveis.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace Aula004.Models
+{
+    public class Imoveis
+    {
+        public string Logradouro { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public string Proprietario { get; set; }
+        public string Telefone { get; set; }
+        public int Valor{ get; set; }
+        public int Area { get; set; }
+        public int Quartos { get; set; }
+        public int Banheiros { get; set; }
+
+        public static List<Imoveis> Lista
+        {
+            get
+            {
+                var lista = new List<Imoveis>
+                {
+                new () { Logradouro = "Alameda Londres, 430" , Bairro = "Jardim Europa", Cidade = "Sorocaba", Estado = "SP", Proprietario = "Alex Machado Ferreira Sampaio", Telefone = "(15)99988-9999", Valor = 500000, Area = 200, Quartos = 4, Banheiros = 3 },   
+                new () { Logradouro = "Rua Montblanc, 0" , Bairro = "Fazenda Boa Vista", Cidade = "Porto Feliz", Estado = "SP", Proprietario = "Jones Artur Gonçalves", Telefone = "(15)99999-9999", Valor = 30000000, Area = 2000, Quartos = 9, Banheiros = 10 },
+
+                };
+                return lista;
+            }
+        }
+    }
+}
